3d44231272fd08f236b3c384439477dcf4e5af51
Jonathan Ng <jonngkh@hotmail.com>
2015-06-22 12:10:41 +0100

    added image to /

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div>
+    <img src='http://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10987379_10153625804937738_1053202883179298049_n.jpg?oh=98b41bcd500268d18fe7c8fd6c588714&oe=562B0E8B'>
+  </div>"
 end
 
-get '/secret' do
-	'This is a secret page'
+get '/secret!!' do
+  'This is a secret page'
 end
